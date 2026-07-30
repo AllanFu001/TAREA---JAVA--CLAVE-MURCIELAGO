ac8f6cfb39ad3f1ec32462cacc2e73459d524f7a
AllanFu001 <aasturiasf@miumg.edu.gt>
2026-07-29 20:57:24 -0600

    VISTA

---
+++ main.java
@@ -0,0 +1,17 @@
+
+package vista;
+
+/**
+ *
+ * @author allan
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
